80f302989f8e7ae976c50e12284c137728e5fc6b
Souradeep Saha <128615880+souradeep1122@users.noreply.github.com>
2026-04-27 00:10:01 +0530

    changing server.js

--- server.js
+++ server.js
@@ -15,9 +15,20 @@ connectDB();
 const app = express();
 
 // ── Middleware ────────────────────────────────
-app.use(cors({
+/*app.use(cors({
   origin: ['http://localhost:5173', 'http://localhost:3001', 'http://localhost:3000'],
   credentials: true,
+}));
+*/
+
+app.use(cors({
+  origin: [
+    "https://busyatrakolkata.netlify.app",
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "http://localhost:3001"
+  ],
+  credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
